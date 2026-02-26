b09e0db7d0f94c0e8e4bd5a5f6fa48ee1b12b3c3
MouserRU <mouserru@yandex.com>
2026-02-26 16:54:10 +0300

    Added query dispatching

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -9,6 +9,11 @@ public class HelloWorld {
             config.bundledPlugins.enableDevLogging();
             // Описываем, что загрузится по адресу /
             config.routes.get("/", ctx -> ctx.result("Hello World"));
+
+            // Пример диспетчеризации запросов
+            config.routes.get("/hello", ctx -> ctx.result("GET /hello"));
+            config.routes.post("/hello", ctx -> ctx.result("POST /hello"));
+
         });
         app.start(7070); // Стартуем веб-сервер
     }
